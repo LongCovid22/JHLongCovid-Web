cf234fee1c8bdfe6423c32da694cb1b969ccc0a7
nate <nfarrell259925@gmail.com>
2022-09-28 14:03:31 -0400

    FIxed project after issue

--- jhlongcovid/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ jhlongcovid/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,18 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "jhlongcovid": {
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "api": {
-        "jhlongcovid": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
