a83df3bf977945476da1a53307b233872368ad16
nate <nfarrell259925@gmail.com>
2022-10-12 18:00:34 -0400

    pushing in-survey MFA

--- jhlongcovid/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ jhlongcovid/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "jhlongcovid": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "jhlongcovid": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- jhlongcovid/components/Survey/SurveyBody/MFA.tsx
+++ jhlongcovid/components/Survey/SurveyBody/MFA.tsx
@@ -13,16 +13,31 @@ import {
   HStack,
   ListIcon,
   FormHelperText,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import { SurveyQuestionProps } from "../SurveyWrapper";
 
 export const MFA: React.FC<SurveyQuestionProps> = ({
   currentQuestion,
   setAnswer,
+  setErrorPresent,
 }) => {
   const [verificationCode, setVerificationCode] = useState("");
+  const [isNum, setIsNum] = useState(true);
 
-  // implement confirmSignUp. setAnswer to success on finish
+  const handleVerificationCode = (code: string) => {
+    const regex = new RegExp(/^\d+$/);
+    if (!regex.test(code)) {
+      setIsNum(false);
+      setErrorPresent && setErrorPresent(true);
+    } else {
+      setIsNum(true);
+      setErrorPresent && setErrorPresent(false);
+    }
+
+    setAnswer(code);
+    setVerificationCode(code);
+  };
 
   return (
     <>
@@ -31,16 +46,21 @@ export const MFA: React.FC<SurveyQuestionProps> = ({
           {currentQuestion.question}
         </Text>
         <Stack direction={"column"} width={"100%"}>
-          <Text>{currentQuestion.options.title}</Text>
-          <Input
-            width={"50%"}
-            type={currentQuestion.options.type}
-            placeholder={currentQuestion.options.placeholder}
-            value={verificationCode}
-            onChange={(event) => {
-              setVerificationCode(event.target.value);
-            }}
-          />
+          <FormControl isInvalid={!isNum}>
+            <FormLabel>{currentQuestion.options.title}</FormLabel>
+            <Input
+              width={"50%"}
+              type={currentQuestion.options.type}
+              placeholder={currentQuestion.options.placeholder}
+              value={verificationCode}
+              onChange={(event) => {
+                handleVerificationCode(event.target.value);
+              }}
+            />
+            {!isNum && (
+              <FormErrorMessage>Please enter a number</FormErrorMessage>
+            )}
+          </FormControl>
         </Stack>
       </VStack>
     </>
