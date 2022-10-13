0856de1d27b3ac9767c11905395108bcce78b438
nate <nfarrell259925@gmail.com>
2022-10-13 16:00:51 -0400

    Got rid of unneccesary dependencies, fixed height issue with charts

--- jhlongcovid/components/LeftSidePanel/LeftSidePanel.tsx
+++ jhlongcovid/components/LeftSidePanel/LeftSidePanel.tsx
@@ -15,7 +15,6 @@ import {
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import styles from "../../styles/LeftSidePanel.module.css";
 import { InfoPanelMetrics } from "../Metrics/InfoPanelMetrics";
-import dynamic from "next/dynamic";
 import { selectHeight, selectWidth } from "../../redux/slices/viewportSlice";
 import {
   Chart as ChartJS,
@@ -27,16 +26,10 @@ import {
   Title,
   Tooltip,
   Legend,
-  ArcElement
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-import { Line } from 'react-chartjs-2';
-import faker from 'faker';
-import { MdSchema } from "react-icons/md";
-// import { VaccinationSummary } from "../../amplify/backend/api/jhlongcovid/schema.graphql"
-
-
+  ArcElement,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import faker from "faker";
 
 ChartJS.register(
   CategoryScale,
@@ -49,17 +42,10 @@ ChartJS.register(
   Legend,
   ArcElement
 );
-const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 interface LeftSidePanelProps {
   data: any;
 }
-
-
-
-// export function LeftSidePanel() {
-//   return <Bar options={options} data={data} />;
-// }
 
 export const LeftSidePanel: React.FC<LeftSidePanelProps> = ({ data }) => {
   const dispatch = useAppDispatch();
@@ -67,62 +53,36 @@ export const LeftSidePanel: React.FC<LeftSidePanelProps> = ({ data }) => {
   const width = useAppSelector(selectWidth);
   const height = useAppSelector(selectHeight);
 
-  const series = [
-    {
-      data: [135, 25, 75],
-    },
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
   ];
-  const options = {
-    xaxis: {
-      categories: ["20-40", "41-60", "61-80"],
-      title: {
-        text: "Age Range (Years)",
-      },
-    },
-    yaxis: {
-      title: {
-        text: "Long COVID Cases",
-      },
-    },
-    title: {
-      text: "Long COVID Count by Age",
-    },
-  };
-
-  // const series2 = [35, 22, 21, 10, 12];
-  // const options2 = {
-  //   labels: [
-  //     "General",
-  //     "Neurologic",
-  //     "Digestive",
-  //     "Respiratory/Heart",
-  //     "Other",
-  //   ],
-  //   title: {
-  //     text: "% of Symptoms in Body System",
-  //   },
-  // };
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July','August'];
 
   const options_1 = {
     labels,
     responsive: true,
     plugins: {
       legend: {
-        position: 'top' as const,
+        position: "top" as const,
       },
       title: {
         display: true,
-        text: 'Long covid count by age',
+        text: "Long covid count by age",
       },
       datalabels: {
-        anchor: 'end',
-        align: 'top',
+        anchor: "end",
+        align: "top",
         formatter: Math.round,
         font: {
-            weight: 'bold',
-            size: 16
-        }
+          weight: "bold",
+          size: 16,
+        },
       },
     },
   };
@@ -131,101 +91,65 @@ export const LeftSidePanel: React.FC<LeftSidePanelProps> = ({ data }) => {
     labels,
     datasets: [
       {
-        label: 'Number of cases',
+        label: "Number of cases",
         data: labels.map(() => faker.datatype.number({ min: 0, max: 500 })),
-        backgroundColor: 'rgba(0, 100, 255, 0.5)',
+        backgroundColor: "rgba(0, 100, 255, 0.5)",
       },
     ],
   };
 
-  // const options_2 = {
-  //   labels,
-  //   responsive: true,
-  //   plugins: {
-  //     legend: {
-  //       position: 'top' as const,
-  //     },
-  //     title: {
-  //       display: true,
-  //       text: 'Cases for different complications',
-  //     },
-  //   },
-  // };
-
-  const labels1 = ['January','April','July',"October"];
+  const labels1 = ["January", "April", "July", "October"];
   const options_3 = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top' as const,
+        position: "top" as const,
       },
       title: {
         display: true,
-        text: 'Vaccination count',
+        text: "Vaccination count",
       },
       datalabels: {
-        anchor: 'end',
-        align: 'top',
+        anchor: "end",
+        align: "top",
         formatter: Math.round,
         font: {
-            weight: 'bold',
-            size: 16
-        }
+          weight: "bold",
+          size: 16,
+        },
       },
     },
   };
 
-
-  const data_2 = {
-    labels,
-    datasets: [
-      {
-        label: 'Complication1',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Complication2',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
-    ],
-  };
-
-  
   const data_3 = {
     labels,
     datasets: [
       {
-        label: 'Pfizer',
+        label: "Pfizer",
         data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
       {
-        label: 'Moderna',
+        label: "Moderna",
         data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
       },
       {
-        label: 'J&J',
+        label: "J&J",
         data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(97, 230, 235, 0.5)',
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(97, 230, 235, 0.5)",
       },
       {
-        label: 'AZ',
+        label: "AZ",
         data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(9, 30, 235, 0.5)',
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(9, 30, 235, 0.5)",
       },
     ],
   };
-
-
 
   return (
     <>
@@ -266,26 +190,8 @@ export const LeftSidePanel: React.FC<LeftSidePanelProps> = ({ data }) => {
             overflowX="hidden"
           >
             <InfoPanelMetrics data={data} />
-            <Chart
-              options={options}
-              series={series}
-              type="bar"
-              width={width * 0.35 < 420 ? 350 : width * 0.35 - 70}
-              height={300}
-            />
-            <Bar 
-              options={options_1} data={data_1} 
-            />;
-            <Bar 
-              options={options_3} data={data_3} 
-            />;
-            {/* <Chart
-              options={options2}
-              series={series2}
-              type="donut"
-              width={width * 0.35 < 420 ? 350 : width * 0.35 - 70}
-              height={300}
-            /> */}
+            <Bar options={options_1} data={data_1} height={200} />;
+            <Bar options={options_3} data={data_3} height={200} />;
             <Spacer />
           </VStack>
         </Box>
