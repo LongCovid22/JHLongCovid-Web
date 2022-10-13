115349f59d139075a3a75e29f6fe2a1592822daa
nate <nfarrell259925@gmail.com>
2022-10-13 16:09:55 -0400

    branch cleanup

--- jhlongcovid/components/Header/ProfileCheckin.tsx
+++ jhlongcovid/components/Header/ProfileCheckin.tsx
@@ -14,36 +14,16 @@ import {
   IconButton,
   Checkbox,
 } from "@chakra-ui/react";
-import { motion } from 'framer-motion';
 import styles from "../../styles/Header.module.css";
-import { SearchIcon } from "@chakra-ui/icons";
-import {Spring} from "react-spring";
-import { EmailIcon } from "@chakra-ui/icons";
 
 import React, { useEffect, useState } from "react";
-
-import { Intro } from "../Survey/Intro";
-
-import { Identifiers } from "../Survey/Identifiers";
-
-import { CovidHistory } from "../Survey/covidHistory";
-
-import { VaccinationHistory } from "../Survey/VaccinationHistory";
-
-import { ThankYou } from "../Survey/ThankYou";
-
-import { EmailRegister } from "../Survey/EmailRegister";
-
-import { MultiFactor } from "../Survey/MultiFactor";
 
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 
 import { selectWidth } from "../../redux/slices/viewportSlice";
 import { SurveyWrapper } from "../Survey/SurveyWrapper";
 
-
 interface ProfileCheckinProps {}
-
 
 const animationKeyframes = keyframes`
 0% {
@@ -78,8 +58,6 @@ interface ProfileCheckinProps {}
 
 function Survey() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const dispatch = useAppDispatch();
   return (
     <>
       <Button
@@ -92,52 +70,13 @@ function Survey() {
       >
         Contribute
       </Button>
-
       <Modal isOpen={isOpen} onClose={onClose} isCentered size={"xl"}>
         <ModalOverlay />
         <SurveyWrapper onClose={onClose} />
-        {/* provide email */}
-        {/* <div style={{ display: number === -2 ? "inline" : "none" }}>
-          <EmailRegister />
-        </div>
-        <div style={{ display: number === -1 ? "inline" : "none" }}>
-          <MultiFactor />
-        </div>
-
-        <div style={{ display: number === 0 ? "inline" : "none" }}>
-          <Intro />
-        </div>
-        <div style={{ display: number === 1 ? "inline" : "none" }}>
-          <Identifiers />
-        </div>
-
-        <div style={{ display: number === 2 ? "inline" : "none" }}>
-          <Demographics />
-        </div>
-
-        <div style={{ display: number === 3 ? "inline" : "none" }}>
-          <CovidHistory />
-        </div>
-
-        <div style={{ display: number === 4 ? "inline" : "none" }}>
-          <PostCovid />
-        </div>
-        <div style={{ display: number === 5 ? "inline" : "none" }}>
-          <Recovery />
-        </div>
-
-        <div style={{ display: number === 6 ? "inline" : "none" }}>
-          <VaccinationHistory />
-        </div>
-
-        <div style={{ display: number === 7 ? "inline" : "none" }}>
-          <ThankYou />
-        </div> */}
       </Modal>
     </>
   );
 }
-
 
 export const ProfileCheckin: React.FC<ProfileCheckinProps> = () => {
   // return(<BasicUsage />)
@@ -154,32 +93,8 @@ export const ProfileCheckin: React.FC<ProfileCheckinProps> = () => {
         top: width < 700 ? "70px" : "0px",
       }}
     >
-
-      {/* <Box
-        as={motion.div}
-        animation={animation}
-        padding="2"
-        bgGradient="linear(to-l, #142d4c, #ffffff)"
-        width="6"
-        height="6"
-        display="flex"
-        /> */}
-      {/* <Button
-        flex={2}
-        borderRadius={"400px"}
-        bgGradient="linear(to-l, blue.300, gray.500)"
-        textColor="white"
-        colorScheme="hopkinsBlue"
-      >
-        Contribute
-  </Button> */}
-
-
       <Survey />
       <Avatar></Avatar>
-
     </Flex>
   );
 };
-
-
