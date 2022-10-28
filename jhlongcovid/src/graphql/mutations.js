d65d649226f1966523fce56a33f1385a4702f521
nate <nfarrell259925@gmail.com>
2022-10-28 13:24:11 -0400

    Fixed schema and added new amplify environment with the changes

--- jhlongcovid/src/graphql/mutations.js
+++ jhlongcovid/src/graphql/mutations.js
@@ -1,6 +1,14 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const aggregateSurveyResults = /* GraphQL */ `
+  mutation AggregateSurveyResults($results: SurveyResults!) {
+    aggregateSurveyResults(results: $results) {
+      statusCode
+      body
+    }
+  }
+`;
 export const createMapData = /* GraphQL */ `
   mutation CreateMapData(
     $input: CreateMapDataInput!
