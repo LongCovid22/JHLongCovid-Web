437c62cf344fdb7aa05ec84ff9e45a1d57cae8c4
hleo1 <leo.hubert3@gmail.com>
2022-11-04 11:55:23 -0400

    figured how to connect to amplify on mock environment

--- jhlongcovid/src/graphql/subscriptions.js
+++ jhlongcovid/src/graphql/subscriptions.js
@@ -1,6 +1,39 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      name
+      description
+      id
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      name
+      description
+      id
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      name
+      description
+      id
+      createdAt
+      updatedAt
+    }
+  }
+`;
 export const onCreateMapData = /* GraphQL */ `
   subscription OnCreateMapData {
     onCreateMapData {
