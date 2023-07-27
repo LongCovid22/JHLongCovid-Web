de19971b8dd4be4eb72b065a83c8b8c1f5a80dd2
hleo1 <leo.hubert3@gmail.com>
2023-07-27 14:53:42 -0400

    finished survey mocking

--- jhlongcovid/components/Survey/__tests__/surveyComponents.test.tsx
+++ jhlongcovid/components/Survey/__tests__/surveyComponents.test.tsx
@@ -1,5 +1,5 @@
 
-import { render, screen, fireEvent,getByLabelText, within } from '@testing-library/react';
+import { render, screen, fireEvent, waitForElementToBeRemoved } from '@testing-library/react';
 import {Provider} from 'react-redux';
 import { createStore } from "../../../redux/store";
 import React from "react";
@@ -21,6 +21,30 @@ function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const clickRadioWithTextAndClickNext = (answer: string): void => {
+  fireEvent.click(screen.getByRole('radio', { name: new RegExp(answer, 'i') }));
+  fireEvent.click(screen.getByText('Next'));
+}
+
+const targetInputWithTestIdAndFillWithValue = (target: string, value: string) => {
+  fireEvent.change(screen.getByTestId(target), { target: { value: value } });
+}
+
+
+const clickScaleRadioButton = (question: string, value: number): void => {
+    const text = screen.getByText(new RegExp(question, 'i'));
+    // Find the div containing "feeling bad about yourself"
+    const feelingBadDiv = text.parentElement;
+    // Find the sibling div (nextElementSibling) that contains the input you want
+    const siblingDiv = feelingBadDiv?.nextElementSibling;
+    // Find the input with value="0" within the sibling div
+    const inputWithValue0 = siblingDiv?.querySelector('input[value="'+ value +'"]');
+    
+    expect(inputWithValue0).toBeInTheDocument(); // Check if the input element is in the document
+    fireEvent.click(inputWithValue0!);
+}
+
+
 describe("Geolocation Test", () => {
   let mockGeolocation;
   beforeEach(() => {
@@ -36,6 +60,18 @@ describe("Geolocation Test", () => {
       clearWatch: jest.fn(),
     };
     (global as any).navigator.geolocation = mockGeolocation;
+    global.scrollTo = jest.fn();
+    // delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+  });
+
+  afterAll(() => {
+    window.ResizeObserver = ResizeObserver;
+    jest.restoreAllMocks();
   });
 
   //needed for env within jest. dotenv includes everything in .env.local in root to process.env
@@ -50,7 +86,6 @@ describe("Geolocation Test", () => {
   });
 
   test('Welcome screen unit test', async () => {
-    
     const test = true;
     const { debug } = render(
       <Provider store={createStore()}>
@@ -74,30 +109,99 @@ describe("Geolocation Test", () => {
     fireEvent.click(screen.getByText('Begin Survey'));
     expect(screen.getByText(/Thank you for considering participation/i)).toBeInTheDocument();
     expect(screen.getByText(/Thank you for your time and your contribution to this crucial/i)).toBeInTheDocument();
-    fireEvent.change(screen.getByPlaceholderText(/Enter Email/i), { target: { value: 'leo.hubert3@gmail.com' } });
+    fireEvent.change(screen.getByPlaceholderText(/Enter Email/i), { target: { value: 'hopkinslongcovidteam@gmail.com' } });
   
     fireEvent.click(screen.getByText('Next'));    
     fireEvent.click(screen.getByText(/Find My Location/i));    
-    expect(await screen.findByText(/New York County, New York/i, {}, { timeout: 2000 })).toBeInTheDocument();
+    expect(await screen.findByText(/New York County, New York/i, {}, { timeout: 4000 })).toBeInTheDocument();
 
     const ageInput = screen.getByTestId('age-input').querySelector('input');
     if(ageInput) {
       fireEvent.change(ageInput, { target: { value: '23' } });
     }
+    targetInputWithTestIdAndFillWithValue('sex-input', 'Male');
+    targetInputWithTestIdAndFillWithValue('height-ft-input', '5');
+    targetInputWithTestIdAndFillWithValue('height-in-input', '10');
+    targetInputWithTestIdAndFillWithValue('weight-input', '150');
+    
+    clickRadioWithTextAndClickNext('Asian');
+    clickRadioWithTextAndClickNext('Yes');
+    clickRadioWithTextAndClickNext('1 infection');
+    clickRadioWithTextAndClickNext('Yes');
+    clickRadioWithTextAndClickNext('1 COVID');
 
-    fireEvent.change(screen.getByTestId('sex-input'), { target: { value: 'Male' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter date'), { target: { value: '2023-11-08' } });
+    fireEvent.click(screen.getByText('Next'));
+    clickRadioWithTextAndClickNext('Yes');
+    clickRadioWithTextAndClickNext('Yes');
+    clickRadioWithTextAndClickNext('Yes');
+    clickRadioWithTextAndClickNext('Not at all');
+    clickRadioWithTextAndClickNext('Yes');
+    
+    fireEvent.click(screen.getByDisplayValue(/Antiviral/i));
+    fireEvent.click(screen.getByText('Next'));
 
-    fireEvent.change(screen.getByTestId('height-ft-input'), { target: { value: '5' } });
-    fireEvent.change(screen.getByTestId('height-in-input'), { target: { value: '10' } });
+    clickRadioWithTextAndClickNext('Yes');
 
-    fireEvent.change(screen.getByTestId('weight-input'), { target: { value: '150' } });
+    fireEvent.change(screen.getByPlaceholderText('Enter number of days'), { target: { value: '5' } });
+    fireEvent.click(screen.getByText('Next'));
+    
+    clickRadioWithTextAndClickNext('Yes');
 
-    const asianRadioButton = screen.getByRole('radio', { name: /Asian/i });
-  fireEvent.click(asianRadioButton);
+    clickRadioWithTextAndClickNext('Do not know');
+
+    clickRadioWithTextAndClickNext('Unsure');
+
+    clickRadioWithTextAndClickNext('Pfizer');
+    clickRadioWithTextAndClickNext('Excellent');
+    clickRadioWithTextAndClickNext('Excellent');
+    clickRadioWithTextAndClickNext('Completely');
+    clickRadioWithTextAndClickNext('None');
 
     fireEvent.click(screen.getByText('Next'));
 
+    fireEvent.click(screen.getByDisplayValue(/Headache/i));
+    fireEvent.click(screen.getByText('Next'));
+
+    clickRadioWithTextAndClickNext('Excellent');
+    clickRadioWithTextAndClickNext('Excellent');
+    clickRadioWithTextAndClickNext('Excellent');
+    clickRadioWithTextAndClickNext('Never');
+
+
+    clickScaleRadioButton('Little interest or pleasure in doing things?', 0);
+    clickScaleRadioButton('Feeling down, depressed, or hopeless?', 0);
+    clickScaleRadioButton('Trouble falling or staying asleep, or sleeping too much?', 0);
+    clickScaleRadioButton('Feeling tired or having little energy?', 0);
+    clickScaleRadioButton('Poor appetite or overeating?', 0);
+    clickScaleRadioButton('Feeling bad about yourself', 0);
+    clickScaleRadioButton('Trouble concentrating on things', 0);
+    clickScaleRadioButton('Moving or speaking so slowly', 0);    
+
+
+    fireEvent.click(screen.getByText('Next'));
+
+    fireEvent.click(screen.getByDisplayValue(/Heart problems/i));
+    fireEvent.click(screen.getByText('Next'));
+
+
+    clickRadioWithTextAndClickNext('Yes');
+    clickRadioWithTextAndClickNext('Yes');
+    clickRadioWithTextAndClickNext('Very difficult');
+    clickRadioWithTextAndClickNext('Working outside of the home');
+
+    fireEvent.click(screen.getByText('Skip'));
+    fireEvent.click(screen.getByText('Finish'));
+
+    await waitForElementToBeRemoved(() => screen.getByText(/Loading/i), {
+      timeout: 10000,
+    });
+
+
     //get whatever data we have already from the store
     debug(undefined, 70000);
-  })
+  }, 20000)
 });
+
+
+
