bfb9c3263d09080e8f2e1d4270d7db67d105fd75
hleo1 <leo.hubert3@gmail.com>
2023-10-27 13:16:25 -0400

    added new deleteMapData lamda function for conveniently accessing delete funciton

--- jhlongcovid/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ jhlongcovid/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -25,6 +25,13 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
+    "deleteSurveyResults": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "emailReceiptConfirmation": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
